9a61f2e9b994b1dd600ea43a7444de64dc297eb1
João Edson Dantas <joaoedson.dantas@outlook.com>
2023-04-09 14:56:12 -0300

    Solucinando alguns bugs do js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,20 @@ const config = document.getElementById("configIcon");
 const btnConfig = document.getElementById("btnSetConfig");
 btnConfig.addEventListener("click", activeFunctions);
 
+/* Variáveis Pomodoro */
+
+let cicloDefaut = 4;
+let segundos;
+let sessoesValue = cicloDefaut;
+let titleSection = document.getElementById("titleSection");
+let sessoes = document.getElementById("sessoes");
+
+/* Variáveis de áudio */
+const audioRegresivo = new Audio("./audio/regresiva.mp3");
+const audioBell = new Audio("./audio/audio_bell.mp3");
+const audioVolta = new Audio("./audio/audio_volta.mp3");
+const audioFinal = new Audio("./audio/audio_final.mp3");
+
 function activeFunctions() {
   /*   setLocal(); */
   startPomdoro();
@@ -12,13 +26,6 @@ function ativarConfig() {
   const modalContainer = document.querySelector(".modalContainer");
   modalContainer.classList.toggle("ativo");
 }
-
-/* Variáveis Pomodoro */
-
-let cicloDefaut = 4;
-let segundos;
-let sessoesValue = cicloDefaut;
-let titleSection = document.getElementById("titleSection");
 
 function setLocal() {
   //localStorage -- Salvar na memória do computador name, valor.
@@ -45,15 +52,20 @@ function setLocal() {
 function startPomdoro() {
   if (localStorage.getItem("acao")) {
     titleSection.innerHTML = "AÇÃO";
+
     //Verificação inicial
-    let sessoes = document.getElementById("sessoes");
-    if (sessoes != 1) {
+
+    if (sessoesValue != 1) {
       sessoes.innerHTML = sessoesValue + " sessões restantes";
     } else {
-      sessoesValue + " sessão restante";
+      sessoes.innerHTML = sessoesValue + " sessão restante";
     }
-    //Relogio
 
+    // CSS do relogio
+    const relogioAcao = document.querySelector(".relogioAcao");
+    relogioAcao.classList.add("ativo");
+    //Relogio
+    audioBell.play();
     let min = Number(localStorage.getItem("acao"));
     min = min - 1;
     segundos = 59;
@@ -75,14 +87,25 @@ function startPomdoro() {
     }
 
     function segTimer() {
-      console.log("Segundo ativo");
       segundos = segundos - 1;
-      document.getElementById("segundos_ok").innerHTML = segundos;
+      document.getElementById("segundos_ok").innerHTML = String(
+        segundos
+      ).padStart(2, 0);
+      if (segundos == 10 && min == 0) {
+        audioRegresivo.play();
+      }
+
       if (segundos <= 0) {
         if (min <= 0) {
           clearInterval(minInterval);
           clearInterval(segInrterval);
-          momentoPausa();
+          audioBell.play();
+
+          if (sessoesValue == 1) {
+            PausaLonga();
+          } else {
+            momentoPausa();
+          }
         }
         segundos = 60;
       }
@@ -95,7 +118,6 @@ function startPomdoro() {
 
 function momentoPausa() {
   titleSection.innerHTML = "DESCANSO";
-  sessoesValue.innerHTML = "Aproveite a sua pausa!";
 
   /* Relogio */
 
@@ -118,20 +140,75 @@ function momentoPausa() {
   function segTimer() {
     console.log("Segundos ativo");
     segundos = segundos - 1;
-    document.getElementById("segundos_ok").innerHTML = segundos;
+    document.getElementById("segundos_ok").innerHTML = String(
+      segundos
+    ).padStart(2, 0);
+    if (segundos == 10 && min == 0) {
+      audioRegresivo.play();
+    }
     if (segundos <= 0) {
       if (minPausa <= 0) {
         clearInterval(minInterval);
         clearInterval(segInrterval);
         sessoesValue = sessoesValue - 1;
 
+        startPomdoro();
+      }
+      segundos = 60;
+    }
+  }
+}
+
+function PausaLonga() {
+  // Adicionado as informações no HMTL
+  titleSection.innerHTML = "DESCANSO LONGO";
+  sessoes.innerHTML = "Aproveite o seu descanso Longo";
+
+  // Pegando o valor da pausa longa e definindo o relogio
+  let minPausalonga = Number(localStorage.getItem("longPause"));
+  minPausalonga = minPausalonga - 1;
+  segundos = 59;
+
+  // Adicionado os minutos no HTML
+  document.getElementById("minutos_ok").innerHTML = minPausalonga;
+  document.getElementById("segundos_ok").innerHTML = segundos;
+
+  // Definindo o tempo de intervalo
+  let minInterval = setInterval(minTimer, 60000); // A cada 60s ative a função minTimer;
+  let segInrterval = setInterval(segTimer, 1000); // A cada 1s ative a function segTimer;
+  // funcão para difinir os mínutos
+  function minTimer() {
+    minPausalonga = minPausalonga - 1;
+    document.getElementById("minutos_ok").innerHTML = minPausalonga;
+  }
+  // funcão para difinir os segundos;
+  function segTimer() {
+    segundos = segundos - 1;
+    document.getElementById("segundos_ok").innerHTML = String(
+      segundos
+    ).padStart(2, 0);
+    if (segundos == 10 && min == 0) {
+      audioRegresivo.play();
+    }
+    if (segundos <= 0) {
+      if (minPausalonga <= 0) {
+        clearInterval(minInterval);
+        clearInterval(segInrterval);
+        sessoesValue = sessoesValue - 1;
         if (sessoesValue <= 0) {
           localStorage.clear();
-          document.getElementById("titleSection").innerHTML = "FIM!";
-          document.getElementById("sessoes").innerHTML =
-            "Parabéns! Você finalizou suas tarefas.";
-        } else {
-          startPomdoro();
+          audioFinal.play();
+          // Alterando o HTML para o final.
+          const titleSection = document.getElementById("titleSection");
+          titleSection.innerHTML = "FIM!";
+          titleSection.classList.add("ativo");
+
+          const sessoes = document.getElementById("sessoes");
+          sessoes.innerHTML = "Parabéns! Você finalizou suas tarefas.";
+          sessoes.classList.add("ativo");
+
+          const timerClicle = document.querySelector(".timerCicle");
+          timerClicle.classList.add("ativo");
         }
       }
       segundos = 60;
